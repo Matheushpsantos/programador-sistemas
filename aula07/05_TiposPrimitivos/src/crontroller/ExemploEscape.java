771d75581ea0a4f46c07851d38ed9c6a99b67324
Matheushpsantos <44246653+Matheushpsantos@users.noreply.github.com>
2018-10-18 21:19:05 -0300

    Add files via upload

---
+++ aula07/05_TiposPrimitivos/src/crontroller/ExemploEscape.java
@@ -0,0 +1,13 @@
+package crontroller;
+
+public class ExemploEscape {
+	public static void main (String[] args) {
+		
+		System.out.println("\t utilizando tab");
+		System.out.println("\n\n2x quebra de linha");
+		System.out.println("\\ contra-barra");
+		System.out.println("\'Aspas Simples'");
+		System.out.println("\"Aspas duplas\"");
+	}
+
+}
---
+++ aula07/05_TiposPrimitivos/src/crontroller/ExemploInteiro.java
@@ -0,0 +1,15 @@
+package crontroller;
+
+public class ExemploInteiro {
+		public static void main (String[] args) {
+	   /// "int" nesse caso faz referencia ao tipo primitivo das variáveis "numero1", "numero2" e "soma"
+			int numero1, numero2, soma;
+			
+			numero1 = 12;
+			numero2 = 3;
+			soma = numero1 + numero2;
+			
+			System.out.println("valor da soma de " + soma);
+		}
+
+}
