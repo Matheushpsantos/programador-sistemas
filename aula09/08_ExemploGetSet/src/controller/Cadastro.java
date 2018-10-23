66fa1a0ed43b63c88db3e93aadeb72f234be11d8
Matheushpsantos <44246653+Matheushpsantos@users.noreply.github.com>
2018-10-22 21:36:05 -0300

    Add files via upload

---
+++ aula09/08_ExemploGetSet/src/controller/Cadastro.java
@@ -0,0 +1,18 @@
+package controller;
+	
+	import model.Pessoa;
+	
+	public class Cadastro {
+		
+		public static void main (String[] args) {
+			Pessoa pessoa = new Pessoa();
+			
+			pessoa.setNome("Leandro Ferra");
+			pessoa.setIdade(28);
+			pessoa.setSexo("masculino");
+			
+			System.out.println("Nome:" + pessoa.getNome());
+			System.out.println("idade: " + pessoa.getIdade());
+			System.out.println("sexo: " + pessoa.getSexo());
+		}
+	}
