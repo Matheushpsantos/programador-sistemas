d21a2fbd55748568dae2608186ec7d7eb73c2026
Matheushpsantos <44246653+Matheushpsantos@users.noreply.github.com>
2018-11-01 20:47:26 -0300

    Add files via upload

---
+++ aula17/017_VerdadeiroFalso/src/controller/Teste.java
@@ -0,0 +1,23 @@
+package controller;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		boolean b = false;
+		boolean a = true;
+		boolean c =  a || b;
+		
+		if(a)
+		{
+			System.out.println(c);
+		}
+		
+		/*if (a == b) {
+			System.out.println("Iguais");
+		} else { 
+			System.out.println("diferentes");
+		}*/
+		
+	}
+
+}
