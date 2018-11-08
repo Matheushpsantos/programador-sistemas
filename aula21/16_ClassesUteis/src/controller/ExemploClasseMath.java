8908eb7ec7199b9cfc662d4d3c45de3e346bd997
Matheushpsantos <44246653+Matheushpsantos@users.noreply.github.com>
2018-11-08 21:39:23 -0200

    Add files via upload

---
+++ aula21/16_ClassesUteis/src/controller/ExemploClasseMath.java
@@ -0,0 +1,21 @@
+package controller;
+
+public class ExemploClasseMath {
+
+	public static void main(String[] args) {
+		
+		// Sorteando um número entre 50 e 80
+		double valSorteado = Math.random();
+		int resultado = 50 + (int) (valSorteado * 30);
+		System.out.println("sorteio entre 50 e 80 --->"+ resultado);
+		
+		// Sorteando a mega-sena
+		for (int i = 0; i < 6; i++) {
+			double numeroSena = Math.random();
+			int numeroSorteado = (int) (numeroSena * 61);
+			System.out.println("");
+			System.out.println("Número Sorteado: "+ numeroSorteado);
+		}
+	}
+
+}
